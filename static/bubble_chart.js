905a232bb66a7885890eeed9a86a6dabc5908ce9
ssimmons9212 <sammsim87@gmail.com>
2020-03-18 18:40:10 -0500

    front end for project 2

---
+++ static/bubble_chart.js
@@ -0,0 +1,57 @@
+chart = {
+  const root = pack(data);
+  
+  const svg = d3.create("svg")
+      .attr("viewBox", [0, 0, width, height])
+      .attr("font-size", 10)
+      .attr("font-family", "sans-serif")
+      .attr("text-anchor", "middle");
+
+  const leaf = svg.selectAll("g")
+    .data(root.leaves())
+    .join("g")
+      .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+
+  leaf.append("circle")
+      .attr("id", d => (d.leafUid = DOM.uid("leaf")).id)
+      .attr("r", d => d.r)
+      .attr("fill-opacity", 0.7)
+      .attr("fill", d => color(d.data.group));
+
+  leaf.append("clipPath")
+      .attr("id", d => (d.clipUid = DOM.uid("clip")).id)
+    .append("use")
+      .attr("xlink:href", d => d.leafUid.href);
+
+  leaf.append("text")
+      .attr("clip-path", d => d.clipUid)
+    .selectAll("tspan")
+    .data(d => d.data.name.split(/(?=[A-Z][a-z])|\s+/g))
+    .join("tspan")
+      .attr("x", 0)
+      .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
+      .text(d => d);
+
+  leaf.append("title")
+      .text(d => `${d.data.title === undefined ? "" : `${d.data.title}
+`}${format(d.value)}`);
+    
+  return svg.node();
+}
+
+data = d3.csvParse(await FileAttachment("C:/Users/samms/Desktop/Test_Website/flare.csv").text(), ({id, value}) => ({name: id.split(".").pop(), title: id.replace(/\./g, "/"), group: id.split(".")[1], value: +value}))
+
+pack = data => d3.pack()
+    .size([width - 2, height - 2])
+    .padding(3)
+  (d3.hierarchy({children: data})
+    .sum(d => d.value))
+
+  width = 932
+  height = width
+
+  format = d3.format(",d")
+
+  color = d3.scaleOrdinal(data.map(d => d.group), d3.schemeCategory10)
+
+  d3 = require("d3@5")
